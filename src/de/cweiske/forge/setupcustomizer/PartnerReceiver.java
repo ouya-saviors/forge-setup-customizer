895ffc17806ad3067d4866182e010cc03fec52e5
Christian Weiske <cweiske@cweiske.de>
2023-10-06 07:18:06 +0200

    Rename package name to de.cweiske.forge.setupcustomizer

--- src/de/cweiske/forge/setupcustomizer/PartnerReceiver.java
+++ src/de/cweiske/forge/setupcustomizer/PartnerReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.android.tvsetup.partnercustomizer;
+package de.cweiske.forge.setupcustomizer;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
